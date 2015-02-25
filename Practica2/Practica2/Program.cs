a1949ce75731acf9bf00c6c0202055c95c625714
Edgar Raul Benitez Garcia <EDGAR_SHAORAN_YUE@hotmail.com>
2015-02-25 10:13:53 -0600

    Iniciando Programa de Tiempo. ---solo se programo la pantalla de presentacion.

---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Opciones de Tiempo");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[1] Cronometro");
+            System.Console.WriteLine("[2] Reloj");
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
