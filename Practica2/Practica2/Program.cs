2a243877de88c989aac00667ac79b8d78896411d
Edgar Raul Benitez Garcia <EDGAR_SHAORAN_YUE@hotmail.com>
2015-02-25 10:52:15 -0600

    Se termino la operacion de CRONOMETRO

--- Practica2/Practica2/Program.cs
+++ Practica2/Practica2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Practica2
 {
@@ -9,11 +10,36 @@ namespace Practica2
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Opciones de Tiempo");
-            System.Console.WriteLine("");
-            System.Console.WriteLine("[1] Cronometro");
-            System.Console.WriteLine("[2] Reloj");
-            System.Console.ReadLine();
+            int horas = 0, minutos = 0, segundos = 0,opc = 0,salir = 0;
+            
+            do
+            {
+                System.Console.WriteLine("Opciones de Tiempo");
+                System.Console.WriteLine("");
+                System.Console.WriteLine("[1] Cronometro");
+                System.Console.WriteLine("[2] Reloj");
+                System.Console.WriteLine("Que deseas ver...");
+                opc = int.Parse(System.Console.ReadLine());
+                switch (opc)
+                {
+                    case 1: System.Console.Clear();
+                       System.Console.WriteLine("Cronometro");
+                        for (horas = 0; horas < 24; horas++)
+                            for (minutos = 0; minutos < 60; minutos++)
+                                for (segundos = 0; segundos < 60; segundos++)
+                                {System.Console.WriteLine("{0}:{1}:{2}", horas, minutos, segundos);
+                                
+                                    
+                           
+                        Thread.Sleep(1000);
+                        System.Console.Clear();}
+ 
+
+
+                        break;
+                }
+            } while (opc != 0);
+            
 
 
         }
